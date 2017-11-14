c833438f0bda343ec921fec4beab354ce87056a1
metel715 <metelin715@gmail.com>
2017-11-14 11:23:39 +0600

    14.11.2017

---
+++ battleship/test1.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+class pole{
+  int my_BORT[10][10];
+  int enemy_BORT[10][10];
+};
+
+class status_kletki{
+  none;    //None
+  kill;    //Kill
+  error;   //Error
+};
+
+void attack(int x, int y){ //function of attack (number of cell)
+  x = ; //getchar 
+  y = ;
+
+}
+
+TestCellStatus(){         //Testing status of cell 
+  if (x > 9 || y > 9) return Error;    //Wrong data 
+  if (my_BORT[x][y] == 0) return None; //Cell is empty
+  if (){                               //Last ship was destroyed 
+    "You are LOST";          
+  }return Kill;
+
+
+}
+
+gameStatus{
+  StartNewGame;
+  Player1Attack;
+  Player2Attack;
+  EndOfGame;
+};
+
+int main(){
+  GameStatus Index = StartNewGame; // Status of game
+  
+  //Create 2 players
+  Player 1;
+  Player 2;
+
+  while(Index)
+
+  return 0;
+}
